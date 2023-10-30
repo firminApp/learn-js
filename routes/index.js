5a96e958dee600bfb6295e0091d087ab1e716f18
kamal <adissakamal0@gmail.com>
2023-10-30 17:56:48 +0100

    added new file

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Learn js page' });
+router.get('/', function(req, res, next) { 
+
+  res.render('index', { title: 'kamal page' });
 });
 
+router.get('/pages', function(req, res, next) { 
+
+  res.render('pages', { title: 'learn js with kamal' });
+});
 module.exports = router;
